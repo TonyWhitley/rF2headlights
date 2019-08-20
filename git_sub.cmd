fd6e0dfb9621c129c3f81e10f245efd864de6a42
TonyWhitley <Tony_Whitley@hotmail.com>
2019-08-20 11:26:47 +0100

    (Add git cmds.  Should update packages to tip)

---
+++ git_sub.cmd
@@ -0,0 +1,8 @@
+echo git bash commands to load submodules
+rem Automated login
+git config --global credential.helper wincred
+
+rem rmdir --ignore-fail-on-non-empty pyDirectInputKeySend
+git submodule add -f https://github.com/TonyWhitley/pyDirectInputKeySend                                                 
+rmdir --ignore-fail-on-non-empty pyRfactor2SharedMemory
+rem git submodule add -f https://github.com/TonyWhitley/pyRfactor2SharedMemory                               
